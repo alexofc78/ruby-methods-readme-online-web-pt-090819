cfd115e44d0fad901c53d2dcc124a55c6bb0bc61
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-09 17:15:34 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -6,14 +6,19 @@ greeting
 
   # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+say_hello_world_ten_times
